f92ea96f72373bb9f161ffa423292572a3aff074
Anthony Tate <anthonydt3817@gmail.com>
2020-06-19 22:37:32 +0000

    Update the README.md file to describe the repo and the pom.xml to contain the project-id.
    Replace starter page with paragraph about me and add images with a button to cycle through them.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+const SNOW_IMG_CAPTION = 'This is from NSBE Nationals last year when I saw ' +
+    'snow for the first time. I later threw on some more jackets, sweats, ' +
+    'gloves, and socks and played with the snow for a couple of hours.';
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+const CHICAGO_IMG_CAPTION = 'Chicago is extremely cold when the weather is ' +
+    'windy. I was very happy to find out the train stations had heaters.';
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+const BABY_IMG_CAPTION = 'This baby picture of me is the only time I know ' +
+    'of that I had longer hair than I do now.';
+
+const MILKSHAKE_IMG_CAPTION = 'Milkshakes are my favorite food/drink. I had ' +
+    'to have a picture with one when I got to school.';
+
+const FISH_IMG_CAPTION = 'I took AP art my senior year of high school. ' +
+    'I\'ve been around photography my whole life so it\'s what I chose ' +
+    'to do for my portfolio. '
+
+function showPictureAndCaption() {
+    const picturesAndCaptions = [['images/Snow.jpg', SNOW_IMG_CAPTION],
+        ['images/Chicago.jpg', CHICAGO_IMG_CAPTION],
+        ['images/Baby.jpg', BABY_IMG_CAPTION],
+        ['images/Milkshake.JPG', MILKSHAKE_IMG_CAPTION],
+        ['images/Fish.PNG', FISH_IMG_CAPTION]];
+
+    const IMG_INDEX = Math.floor(Math.random() * 5);
+
+    const IMG_ELEMENT = document.createElement('img');
+    IMG_ELEMENT.src = picturesAndCaptions[IMG_INDEX][0];
+    const IMG_CAPTION = picturesAndCaptions[IMG_INDEX][1];
+
+    const PICTURE_CONTAINER = document.getElementById('picture');
+    PICTURE_CONTAINER.innerHTML = '';
+    PICTURE_CONTAINER.appendChild(IMG_ELEMENT);
+
+    const CAPTION_CONTAINER = document.getElementById('caption');
+    CAPTION_CONTAINER.innerHTML = '';
+    CAPTION_CONTAINER.innerHTML = IMG_CAPTION;
 }
+
